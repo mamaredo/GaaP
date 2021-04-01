c9939bc6ba9f119ad3c48bab757dab99748118bc
mamaredo_d <11kakeru30@gmail.com>
2021-04-01 16:05:02 +0900

    footerのresponsiveデザインを修正

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,15 @@ import * as React from 'react'
 
 const Footer: React.FC = () => (
   <footer className="w-full bg-gray-300">
-    <div className="container">
+    <div
+      className="
+        2xl:max-w-2xl
+        xl:max-w-xl
+        md:max-w-md
+        max-w-sm
+        mx-auto
+      "
+    >
       from footer
     </div>
   </footer>
