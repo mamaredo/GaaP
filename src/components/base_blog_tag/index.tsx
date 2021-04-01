b94ad06b71f971051df0a8ba596c821b8c6ed729
mamaredo_d <11kakeru30@gmail.com>
2021-04-01 16:08:29 +0900

    BaseBlogTagの作成

--- src/components/base_blog_tag/index.tsx
+++ src/components/base_blog_tag/index.tsx
@@ -1,8 +1,26 @@
 import * as React from "react"
 
+
 // ______________________________________________________
 //
-const BaseBlogTag: React.FC = () => <div>from base blog tag</div>
+type BaseBlogTagProps = {
+  icon: SVGAElement | string
+  text: string
+  style?: string[]
+}
+// ______________________________________________________
+//
+// ______________________________________________________
+//
+const BaseBlogTag: React.FC<BaseBlogTagProps> = ({icon, text, style}) => {
+  const inherentStyle = style?.join(' ')
+  return (
+    <button className={`flex justify-center items-center rounded min-h-button ${inherentStyle}`}>
+      <svg className="ml-icon w-button-icon h-button-icon bg-gray-100">{icon}</svg>
+      <span className="pl-1 pr-2 font-semibold opacity-90">{text}</span>
+    </button>
+  )
+}
 // ______________________________________________________
 //
 export default BaseBlogTag
