db308d43b08b946770f69ce9883804d35041c4a8
mamaredo_d <11kakeru30@gmail.com>
2021-04-01 16:09:06 +0900

    roudedの付与とsocialボタンの追加

--- src/components/base_profile_card/index.tsx
+++ src/components/base_profile_card/index.tsx
@@ -1,19 +1,40 @@
 import * as React from "react"
 
+
+// ______________________________________________________
+//
+// type BaseProfileCardProps = {
+//   avatar: string,
+//   userName: string,
+//   profile: string,
+//   twitter: string,
+//   gitHub: string,
+// }
+// ______________________________________________________
+//
 // ______________________________________________________
 //
 const BaseProfileCard: React.FC = () => (
-  <div className="shadow bg-white w-full h-full p-2">
+  <div className="shadow bg-white w-full p-2 rounded">
     <div className="flex">
       {/* img */}
       <div className="rounded-full bg-gray-200" style={{ width: '56px', height: '56px' }}></div>
       {/* img */}
-      <div>
-        <div className="text-xl">Nishimura</div>
-        <div>2000年 11月 30日 生まれ</div>
+      <div className="ml-2">
+        <div className="text-2xl font-bold opacity-90">Nishimura</div>
+        <div className="text-sm opacity-50">2000年 11月 30日 生まれ</div>
       </div>
     </div>
-    from base profile card
+
+    <div className="mt-2 text-xl font-medium opacity-90">description</div>
+
+    <div>
+      <div className="mt-3 w-full min-h-button flex justify-between">
+        <button className="mr-1 w-full bg-twitter text-white font-semibold rounded">Twitter</button>
+        <button className="ml-1 w-full bg-git-hub text-white font-semibold rounded">GitHub</button>
+      </div>
+      <button className="mt-2 w-full min-h-button bg-content-full text-white font-semibold rounded">CONTACT FROM</button>
+    </div>
   </div>
 )
 // ______________________________________________________
