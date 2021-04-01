9f2b1f5afe6f92d26193fe35d3947b08ec877889
mamaredo_d <11kakeru30@gmail.com>
2021-04-01 16:06:01 +0900

    base_blog_tagからtagのcomponentをimport 記事タイトルと時間のデフォルト値を追加

--- src/components/base_blog_card/index.tsx
+++ src/components/base_blog_card/index.tsx
@@ -1,7 +1,10 @@
 import * as React from "react"
+import BaseBlogTag from "../base_blog_tag"
+
 
 // ______________________________________________________
 //
+/* contentfulとの連携が済めば記事のタグのprops(タグアイコン, タグテキスト)を定義する */
 type BaseBlogCardProps = {
   frontmatter?: {
     title?: string
@@ -20,19 +23,17 @@ const BaseBlogCard: React.FC<BaseBlogCardProps> = ({ style, frontmatter }) => {
 
   return (
     <div className="col-span-4">
-      <div className={`min-h-card shadow bg-white w-full ${cardStyle}`}>
+      <div className={`shadow bg-white w-full rounded ${cardStyle}`}>
         {/* imageを入れる */}
-        <div className="w-full min-h-image bg-pink-200"></div>
+        <div className="w-full min-h-image bg-pink-200 rounded-t"></div>
         {/* imageを入れる */}
         <div className="p-2 flex flex-col">
           <div className="flex justify-between items-center">
-            <h2 className="text-xl">{frontmatter?.title}</h2>
-            <p className="2xl:text-sm text-xs">{frontmatter?.date}</p>
+            <h2 className="text-xl opacity-90">{frontmatter?.title || '記事タイトル'}</h2>
+            <p className="2xl:text-sm text-xs opacity-70">{frontmatter?.date || '2021-03-03'}</p>
           </div>
           <div className="flex flex-row-reverse">
-            <div className="rounded-full py-1 px-2 2xl:text-base text-sm bg-gray-300">
-              {frontmatter?.tag}
-            </div>
+            <BaseBlogTag icon="" text={'TAG NAME'} style={['bg-tag-bg']} />
           </div>
         </div>
       </div>
