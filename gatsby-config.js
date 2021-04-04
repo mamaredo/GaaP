07b8d14e27153a09b5355b90cf8bb94ac9f09668
mamaredo_d <11kakeru30@gmail.com>
2021-04-04 22:47:23 +0900

    gatsby-source-contentfullを追加（preview mode）

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,11 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`
+})
+
 module.exports = {
   siteMetadata: {
     title: `GaaP`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `blog to grow as a programmer.`,
     author: `@gatsbyjs`,
   },
   plugins: [
@@ -55,6 +59,14 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        host: `preview.contentful.com`
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
