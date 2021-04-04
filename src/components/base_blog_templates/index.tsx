8a350918bdef02990f6b77b04cfe426b8548caa3
mamaredo_d <11kakeru30@gmail.com>
2021-04-04 22:50:50 +0900

    blog-head, blog-index componentを追加

--- src/components/base_blog_templates/index.tsx
+++ src/components/base_blog_templates/index.tsx
@@ -1,24 +1,54 @@
 import * as React from "react"
+import BaseNavigation from "../base_navigation"
+import BlogHead from "./blog-head"
+import BlogIndex from "./blog-index"
 import { BlogTemplateProps } from "../../containers/blog_templates"
+import "../../css/blog-template.css"
 
 import Layout from "../layout"
 
 // ______________________________________________________
 //
 type BaseBlogTemplateProps = {
-  frontmatter: BlogTemplateProps["pageContext"]
+  post: BlogTemplateProps["pageContext"]["post"]
+  body: BlogTemplateProps["pageContext"]["body"]
 }
+export type BlogHeadProps = { post: BaseBlogTemplateProps["post"] }
 // ______________________________________________________
 //
-const BaseBlogTemplate: React.FC<BaseBlogTemplateProps> = ({ frontmatter }) => {
-  const { title, date, tag } = frontmatter
+const BaseBlogTemplate: React.FC<BaseBlogTemplateProps> = ({ post, body }) => {
+  const { title, date, tag } = post
   return (
     <Layout>
-      <div>
-        aa
-        <h2>{title}</h2>
-        <p>{tag}</p>
-        <p>{date}</p>
+      <BaseNavigation />
+      <div className="px-1 w-full bg-background">
+        <div 
+          className="
+            pt-32
+            2xl:max-w-2xl
+            xl:max-w-xl
+            md:max-w-md
+            max-w-sm
+            mx-auto
+            grid
+            xl:grid-cols-12
+            md:grid-cols-8
+            grid-cols-4
+            2xl:gap-3
+            gap-2
+          "
+        >
+
+          <div className="xl:col-span-10 md:col-span-8 col-span-4 w-full">
+            <BlogHead title={title} date={date} />
+            <div className="contentfull xl:mt-5 mt-4" dangerouslySetInnerHTML={{ __html: body }} />
+          </div>
+
+          <div className="xl:col-span-2 col-span-4">
+            <BlogIndex />
+          </div>
+
+        </div>
       </div>
     </Layout>
   )
