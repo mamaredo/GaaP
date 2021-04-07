77fb701dcedff5830ba8769d304cc9bb537db2d0
mamaredo_d <11kakeru30@gmail.com>
2021-04-07 17:18:06 +0900

    heroImageを追加、HeroImage componentをimport

--- src/components/base_blog_templates/blog-head.tsx
+++ src/components/base_blog_templates/blog-head.tsx
@@ -1,24 +1,29 @@
 import * as React from "react"
+import HeroImage from "./hero-image"
+import { BaseBlogTemplateProps } from "./index"
 
 
 // ______________________________________________________
 //
 type BlogHeadProps = {
-  title: string,
-  date: string
+  title: BaseBlogTemplateProps["post"]["title"]
+  date: BaseBlogTemplateProps["post"]["date"]
+  image: BaseBlogTemplateProps["image"]
 }
 // ______________________________________________________
 //
 // ______________________________________________________
 //
-const BlogHead: React.FC<BlogHeadProps> = ({title, date}) => {
+const BlogHead: React.FC<BlogHeadProps> = ({ title, date, image }) => {
   return (
     <React.Fragment>
       <span className="text-black text-opacity-50">{date}</span>
       <div className="mt-1 w-full">
-        <h2 className="text-black text-opacity-90 xl:text-5xl md:text-4xl text-3xl">{title}</h2>
+        <h2 className="text-black text-opacity-90 xl:text-5xl md:text-4xl text-3xl font-black">
+          {title}
+        </h2>
       </div>
-      {/* img */}
+      <HeroImage image={image} />
     </React.Fragment>
   )
 }
