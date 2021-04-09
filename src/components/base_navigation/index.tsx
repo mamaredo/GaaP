21297b3044afae1d8a26020d34edaf038c94b3c7
mamaredo_n <11kakeru30@gmail.com>
2021-04-09 12:15:18 +0900

    navigationをfixedを削除

--- src/components/base_navigation/index.tsx
+++ src/components/base_navigation/index.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 
 const BaseNavigation: React.FC = () => {
   return (
-    <nav className="fixed z-50 shadow-lg w-full h-20 flex items-center justify-center bg-primary text-white">
+    <nav className="w-full h-20 flex items-center justify-center bg-background">
       <h1 className="text-3xl">
         GaaP
       </h1>
