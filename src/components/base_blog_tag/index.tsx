0604640f179cf951423980b8286fc55942c43870
mamaredo_d <11kakeru30@gmail.com>
2021-04-13 19:37:19 +0900

    propsの型の修正とsvg iconの表示の仕方を変更

--- src/components/base_blog_tag/index.tsx
+++ src/components/base_blog_tag/index.tsx
@@ -1,26 +1,24 @@
 import * as React from "react"
+import { BlogTagProps } from "../../containers/blog_tag"
 
 
 // ______________________________________________________
 //
 type BaseBlogTagProps = {
-  icon: SVGAElement | string
-  text: string
-  style?: string[]
+  icon: BlogTagProps['icon']
+  tagName: BlogTagProps['tagName']
+  inherentStyle?: string
 }
 // ______________________________________________________
 //
 // ______________________________________________________
 //
-const BaseBlogTag: React.FC<BaseBlogTagProps> = ({icon, text, style}) => {
-  const inherentStyle = style?.join(' ')
-  return (
-    <button className={`flex justify-center items-center rounded min-h-button ${inherentStyle}`}>
-      <svg className="ml-icon w-button-icon h-button-icon bg-gray-100">{icon}</svg>
-      <span className="pl-1 pr-2 font-semibold opacity-90">{text}</span>
-    </button>
-  )
-}
+const BaseBlogTag: React.FC<BaseBlogTagProps> = ({ icon, tagName, inherentStyle }) => (
+  <button className={`flex justify-center items-center rounded min-h-button ${inherentStyle}`}>
+    <div className="ml-icon min-w-tag" dangerouslySetInnerHTML={{ __html: icon }} />
+    <span className="pl-1 pr-2 text-sm font-extrabold opacity-90">{tagName}</span>
+  </button>
+)
 // ______________________________________________________
 //
 export default BaseBlogTag
