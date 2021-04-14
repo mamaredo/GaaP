77a9c418cc99175d40abf1eda0c738448f9c93f8
mamaredo_d <11kakeru30@gmail.com>
2021-04-14 20:48:38 +0900

    tag-page.tsxを作成

---
+++ src/template/tag-page.tsx
@@ -0,0 +1,13 @@
+import * as React from "react"
+
+
+// ______________________________________________________
+//
+const TagPage: React.FC = () => {
+  return (
+    <div>from tag page</div>
+  )
+}
+// ______________________________________________________
+//
+export default TagPage
