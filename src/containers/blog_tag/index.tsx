62c68ac65f66013a6ecf2be6a866c71eff455e1c
mamaredo_d <11kakeru30@gmail.com>
2021-04-14 20:51:09 +0900

    プロップスを修正

--- src/containers/blog_tag/index.tsx
+++ src/containers/blog_tag/index.tsx
@@ -15,9 +15,8 @@ export type BlogTagProps = {
 //
 const BlogTag: React.FC<BlogTagProps> = ({ icon, tagName, style }) => {
   /* logic */
-  const inherentStyle = style?.join(' ')
   return (
-    <BaseBlogTag icon={icon} tagName={tagName} inherentStyle={inherentStyle} />
+    <BaseBlogTag icon={icon} tagName={tagName} inherentStyle={style} />
   )
 }
 // ______________________________________________________
