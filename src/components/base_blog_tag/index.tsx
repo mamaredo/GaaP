a06348ba944516034ecc7be6da5e57a3569fd994
mamaredo_d <11kakeru30@gmail.com>
2021-04-17 23:13:43 +0900

    型の更新

--- src/components/base_blog_tag/index.tsx
+++ src/components/base_blog_tag/index.tsx
@@ -4,24 +4,25 @@ import { Link } from "gatsby"
 
 // ______________________________________________________
 //
-type BaseBlogTagProps = {
-  icon: string
-  tagName: string
-  inherentStyle?: string[]
+export type TagType = {
+  svg: string
+  tag: string
 }
+export type StyleType = { style?: string[] }
+type BaseBlogTagProps = TagType & StyleType
 // ______________________________________________________
 //
 // ______________________________________________________
 //
-const BaseBlogTag: React.FC<BaseBlogTagProps> = ({ icon, tagName, inherentStyle }) => {
-  const style = inherentStyle?.join(' ')
+const BaseBlogTag: React.FC<BaseBlogTagProps> = ({ svg, tag, style }) => {
+  const inherentStyle = style?.join(' ')
   return (
     <Link
-      className={`flex justify-center items-center rounded min-h-button ${style}`}
-      to={`/tags/${tagName}`}
+      className={`flex justify-center items-center rounded min-h-button ${inherentStyle}`}
+      to={`/tags/${tag}`}
     >
-      <div className="ml-icon min-w-tag" dangerouslySetInnerHTML={{ __html: icon }} />
-      <span className="pl-1 pr-2 text-sm font-extrabold opacity-90">{tagName}</span>
+      <div className="ml-icon min-w-tag" dangerouslySetInnerHTML={{ __html: svg }} />
+      <span className="pl-1 pr-2 text-sm font-extrabold opacity-90">{tag}</span>
     </Link>
   )
 }
