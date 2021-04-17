083fdc455b42b257f91a8b47d9e118d5443554f8
mamaredo_d <11kakeru30@gmail.com>
2021-04-17 23:19:32 +0900

    記事カードへのロジックはできたが修正の余地あり

---
+++ src/containers/all_blog_cards/index.tsx
@@ -0,0 +1,30 @@
+import * as React from "react"
+import { useAllBlogPostsQuery } from "./query"
+import BaseBlogCard from "../../components/base_blog_card"
+
+
+
+// ______________________________________________________
+//
+const AllBlogCards: React.FC = () => {
+  const { edges } = useAllBlogPostsQuery()
+  return (
+    <>
+    {
+      edges.map(({node}, index) => (
+        <BaseBlogCard
+          key={index}
+          svg={node.svgContent?.svg?.content || ''}
+          tag={node.tags?.[0] || 'TAGNAME'}
+          slug={node.slug || '404'}
+          title={node.title || 'Title'}
+          date={node.updatedAt || 'YYYY-MM-DD'}
+        />
+      ))
+    }
+    </>
+  )
+}
+// ______________________________________________________
+//
+export default AllBlogCards
--- src/containers/blog_cards/index.tsx
+++ src/containers/blog_cards/index.tsx
@@ -1,23 +1,34 @@
 import * as React from "react"
-// import { useBlogIndexQuery } from "./query"
-import BaseBlogCard from "../../components/base_blog_card"
+import { TagType } from "../../components/base_blog_tag"
+import BaseBlogCard, { BlogCardType } from "../../components/base_blog_card"
+
 
 // ______________________________________________________
 //
-const BlogCards: React.FC = () => {
-  // const { edges } = useBlogIndexQuery()
+export type BlogCardsType = {
+  blogCardInfo: BlogCardType[]
+}
+export type BlogCardsProps = TagType & BlogCardsType
+// ______________________________________________________
+//
+// ______________________________________________________
+//
+const BlogCards: React.FC<BlogCardsProps> = ({ tag, svg, blogCardInfo }) => {
   return (
     <>
-      {/* {edges.map(({ node }, index) => {
-        return (
+    {
+      blogCardInfo.map((el, index) => (
           <BaseBlogCard
             key={index}
-            frontmatter={node?.frontmatter}
-            slug={node.fields?.slug}
+            slug={el.slug}
+            title={el.title}
+            date={el.date}
+            svg={svg}
+            tag={tag}
           />
         )
-      })} */}
-      <BaseBlogCard />
+      )
+    }
     </>
   )
 }
