8be1fae93b62b43371d677a8ace8c6ec6b6c85d4
mamaredo_n <11kakeru30@gmail.com>
2021-04-22 12:02:58 +0900

    twitter, githubのリンク作成

--- src/components/base_profile_card/profile-social.tsx
+++ src/components/base_profile_card/profile-social.tsx
@@ -7,12 +7,16 @@ import { socialType } from "./index"
 const ProfileSocial: React.FC<socialType> = ({ twitter, gitHub, contact }) => (
   <>
     <div className="mt-3 w-full min-h-button flex justify-between">
-      <button className="mr-1 w-full bg-twitter text-white font-semibold rounded">
-        Twitter
-      </button>
-      <button className="ml-1 w-full bg-git-hub text-white font-semibold rounded">
-        GitHub
-      </button>
+      <a href={twitter} className="flex items-center justify-center mr-1 w-full bg-twitter text-white font-semibold rounded">
+        <span>
+          Twitter
+        </span>
+      </a>
+      <a href={gitHub} className="flex items-center justify-center ml-1 w-full bg-git-hub text-white font-semibold rounded">
+        <span>
+          GitHub
+        </span>
+      </a>
     </div>
     <button className="mt-2 w-full min-h-button bg-content-full text-white font-semibold rounded">
       CONTACT FROM
