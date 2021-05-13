f16c5712f2c5e030b97fbe88be0d7e2de766ccb8
mamaredo_d <11kakeru30@gmail.com>
2021-05-13 15:38:06 +0900

    BaseBlogIndexFab.tsxを作成

---
+++ src/components/base-blog-index/base-blog-index-fab/index.tsx
@@ -0,0 +1,33 @@
+import React from "react"
+import FabContent from "./fab-content"
+import { BaseBlogIndexProps } from "../index"
+
+
+// ______________________________________________________
+//
+const BaseBlogIndexFab: React.VFC<BaseBlogIndexProps> = ({ postIndex, isShow, handleIsShow}) => (
+  <>
+    <button
+      className="
+        fixed
+        bottom-2 right-2
+        flex items-center justify-center
+        w-7 h-7
+        rounded-full
+        focus:outline-none
+        bg-secondary
+        shadow-md
+        z-20
+      "
+      onClick={handleIsShow}
+    >
+      <img src={`../../../list-white-24dp.svg`} alt="blog-index-fab" />
+    </button>
+    <div style={{ display: isShow ? 'none' : 'inline' }}>
+      <FabContent postIndex={postIndex} />
+    </div>
+  </>
+)
+// ______________________________________________________
+//
+export default BaseBlogIndexFab
