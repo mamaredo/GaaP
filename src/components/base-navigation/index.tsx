6e73db07417b6a0c4b4926f9019aadd0f1b941d0
mamaredo_d <11kakeru30@gmail.com>
2021-05-13 15:40:48 +0900

    top-pageへのLinkタグを追加

--- src/components/base-navigation/index.tsx
+++ src/components/base-navigation/index.tsx
@@ -1,20 +1,22 @@
-import * as React from "react"
+import React from "react"
+import { Link } from "gatsby"
+
 
 // ______________________________________________________
 //
-const BaseNavigation: React.VFC = () => {
-  return (
-    <div className="w-full h-7">
-      <nav className="w-full h-7 flex items-center bg-primary shadow-md fixed z-20">
-        <div className="flex items-centeer justify-between box-border xl:px-3 px-2 2xl:max-w-2xl lg:max-w-xl max-w-md w-full mx-auto">
+const BaseNavigation: React.VFC = () => (
+  <div className="w-full h-7">
+    <nav className="w-full h-7 flex items-center bg-primary shadow-md fixed z-20">
+      <div className="flex items-centeer justify-center box-border xl:px-3 px-2 2xl:max-w-2xl lg:max-w-xl max-w-md w-full mx-auto">
+        <Link to="/">
           <h1 className="block text-2xl leading-10 text-black text-opacity-90 font-black">
             GaaP
           </h1>
-        </div>
-      </nav>
-    </div>
-  )
-}
+        </Link>
+      </div>
+    </nav>
+  </div>
+)
 // ______________________________________________________
 //
 export default BaseNavigation
