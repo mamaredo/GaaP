2533c133f3294238a290d979d6edcc4f3c7c12a2
mamaredo_n <11kakeru30@gmail.com>
2021-05-25 14:55:30 +0900

    GatsbyImageを追加

--- src/components/base-profile-card/profile-avatar.tsx
+++ src/components/base-profile-card/profile-avatar.tsx
@@ -1,18 +1,25 @@
 import * as React from "react"
 import { ProfileType } from "./index"
+import { GatsbyImage } from "gatsby-plugin-image"
 
 
 // ______________________________________________________
 //
-type ProfileAvatarProps = { avatar: ProfileType["avatar"] }
+type ProfileAvatarProps = {
+  avatar: ProfileType["avatar"]
+  alt: ProfileType["name"]
+}
 // ______________________________________________________
 //
 // ______________________________________________________
 //
-const ProfileAvatar: React.FC<ProfileAvatarProps> = ({ avatar }) => (
-  <div className="rounded bg-gray-200 h-1/2">
-    {avatar}
-  </div>
+const ProfileAvatar: React.FC<ProfileAvatarProps> = ({ avatar, alt }) => (
+  <GatsbyImage
+    className="rounded-full"
+    image={avatar}
+    alt={alt}
+  >
+  </GatsbyImage>
 )
 // ______________________________________________________
 //
