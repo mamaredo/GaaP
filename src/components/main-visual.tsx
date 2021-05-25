4eea594b0f4b012b12fcb76413a1dcda531d20a3
mamaredo_n <11kakeru30@gmail.com>
2021-05-25 14:56:05 +0900

    メインビジュアルのコンポーネントを作成

---
+++ src/components/main-visual.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+
+// ______________________________________________________
+//
+const MainVisual: React.VFC = () => (
+  <header className="w-full h-main-visual min-h-image flex justify-center items-center bg-primary">
+    <div>
+      from header
+    </div>
+  </header>
+)
+// ______________________________________________________
+//
+export default MainVisual
