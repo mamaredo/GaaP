b2a39d9029b4bf0555b921fbff63b8d311702d35
mamaredo_n <11kakeru30@gmail.com>
2021-05-25 14:54:49 +0900

    cmsからavatar画像を受け取る処理を追加

--- src/containers/profile-card/index.tsx
+++ src/containers/profile-card/index.tsx
@@ -1,18 +1,19 @@
 import * as React from "react"
 import { useProfileQuery } from "./query"
-import BaseProfileCard from "../../components/base-profile-card"
+import BaseProfileCard, { ProfileType, SocialType} from "../../components/base-profile-card"
+import { IGatsbyImageData } from "gatsby-plugin-image"
 
 
 // ______________________________________________________
 //
-const ProfileCard: React.FC = () => {
+const ProfileCard: React.VFC = () => {
   const data = useProfileQuery()
-  const profile = {
-    avatar: 'avatar',
+  const profile: ProfileType = {
+    avatar: data?.icon?.gatsbyImageData as unknown as IGatsbyImageData,
     name: data?.name || 'UserName',
     bio: data?.bio?.childrenMarkdownRemark?.[0]?.html || 'Bio',
   }
-  const social = {
+  const social: SocialType = {
     twitter: data?.twitter || '#',
     gitHub: data?.gitHub || '#',
     contact: data?.contact || '#'
