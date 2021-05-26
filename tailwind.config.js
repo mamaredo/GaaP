20a1c1221d877ea09adbda1a8e42c9d7a01709b2
mamaredo_d <11kakeru30@gmail.com>
2021-05-26 12:37:22 +0900

    google fontsのfont familyを追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      fontFamily: {
+        'balsamiq': ['"Balsamiq Sans"', 'cursive']
+      },
       colors: {
         'primary': '#60CAAD',
         'secondary': '#333333',
