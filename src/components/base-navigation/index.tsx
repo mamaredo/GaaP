59ddd720af3f1ae717a8b0959db15797231bc612
mamaredo_d <11kakeru30@gmail.com>
2021-05-26 12:04:38 +0900

    ナビゲーションにアイコンを追加

--- src/components/base-navigation/index.tsx
+++ src/components/base-navigation/index.tsx
@@ -6,12 +6,19 @@ import { Link } from "gatsby"
 //
 const BaseNavigation: React.VFC = () => (
   <div className="w-full h-7">
-    <nav className="w-full h-7 flex items-center bg-primary shadow-md fixed z-20">
-      <div className="flex items-centeer justify-center box-border xl:px-3 px-2 2xl:max-w-2xl lg:max-w-xl max-w-md w-full mx-auto">
+    <nav className="w-full h-7 box-border px-1 flex items-center bg-primary shadow-md fixed z-20">
+      <div
+        className="
+          2xl:max-w-2xl lg:max-w-xl max-w-md
+          w-full
+          mx-auto
+        "
+      >
         <Link to="/">
-          <h1 className="block text-2xl leading-10 text-black text-opacity-90 font-black">
+          <h2 className="flex items-center md:text-3xl text-2xl font-bold text-header-text">
+            <img className="w-6 h-6 mr-icon" src={`../../../gaap-logp.svg`} alt="GaaP" />
             GaaP
-          </h1>
+          </h2>
         </Link>
       </div>
     </nav>
