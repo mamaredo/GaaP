16d2ed0543528cfa85750989719a26d4dc1a712b
mamaredo_d <11kakeru30@gmail.com>
2021-05-27 17:26:00 +0900

    google form のお問い合わせフォームを追加

--- src/components/base-profile-card/profile-social.tsx
+++ src/components/base-profile-card/profile-social.tsx
@@ -1,11 +1,11 @@
-import * as React from "react"
+import React from "react"
 import { SocialType } from "./index"
 
 
 
 // ______________________________________________________
 //
-const ProfileSocial: React.FC<SocialType> = ({ twitter, gitHub, contact }) => (
+const ProfileSocial: React.VFC<SocialType> = ({ twitter, gitHub, contact }) => (
   <>
     <div className="mt-3 w-full min-h-button flex justify-between">
       <a href={twitter} className="flex items-center justify-center mr-1 w-full bg-twitter text-white font-semibold rounded">
@@ -21,12 +21,14 @@ const ProfileSocial: React.FC<SocialType> = ({ twitter, gitHub, contact }) => (
         </span>
       </a>
     </div>
-    <button className="flex items-center justify-center mt-2 w-full min-h-button bg-content-full text-white font-semibold rounded">
-      <img className="min-w-tag mr-1" src={`../../../netlify-icon.svg`} />
+    <a
+      href={contact}
+      className="flex items-center justify-center mt-2 w-full min-h-button bg-google-form text-white font-semibold rounded"
+    >
       <span>
         CONTACT FROM
       </span>
-    </button>
+    </a>
   </>
 )
 // ______________________________________________________
