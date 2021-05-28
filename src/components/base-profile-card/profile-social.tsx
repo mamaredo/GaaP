e27574ab6364d0de9fc4f452e4fdd170e103635a
mamaredo_n <11kakeru30@gmail.com>
2021-05-28 11:39:37 +0900

    social iconの変更

--- src/components/base-profile-card/profile-social.tsx
+++ src/components/base-profile-card/profile-social.tsx
@@ -8,14 +8,14 @@ import { SocialType } from "./index"
 const ProfileSocial: React.VFC<SocialType> = ({ twitter, gitHub, contact }) => (
   <>
     <div className="mt-3 w-full min-h-button flex justify-between">
-      <a href={twitter} className="flex items-center justify-center mr-1 w-full bg-twitter text-white font-semibold rounded">
-        <img className="min-w-tag max-w-tag mr-1" src={`../../../twitter-icon.svg`} alt="twitter-icon" />
+      <a href={twitter} className="flex items-center justify-center mr-1 w-full bg-white text-black text-opacity-90 font-semibold rounded shadow-md">
+        <img className="min-w-tag max-w-tag mr-icon" src={`../../../twitter-icon.svg`} alt="twitter-icon" />
         <span>
           Twitter
         </span>
       </a>
-      <a href={gitHub} className="flex items-center justify-center ml-1 w-full bg-git-hub text-white font-semibold rounded">
-        <img className="min-w-tag max-w-tag mr-1" src={`../../../git-hub-icon.svg`} alt="git-hub-icon" />
+      <a href={gitHub} className="flex items-center justify-center ml-1 w-full bg-white text-git-hub font-semibold rounded shadow-md">
+        <img className="min-w-tag max-w-tag mr-icon" src={`../../../git-hub-icon.svg`} alt="git-hub-icon" />
         <span>
           GitHub
         </span>
@@ -23,11 +23,9 @@ const ProfileSocial: React.VFC<SocialType> = ({ twitter, gitHub, contact }) => (
     </div>
     <a
       href={contact}
-      className="flex items-center justify-center mt-2 w-full min-h-button bg-google-form text-white font-semibold rounded"
+      className="flex items-center justify-center mt-2 w-full min-h-button bg-google-form text-white font-semibold rounded shadow-md"
     >
-      <span>
-        CONTACT FORM
-      </span>
+      CONTACT FORM
     </a>
   </>
 )
