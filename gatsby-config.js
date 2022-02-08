4b1b607fcca0d94782c41c94977432b5e3960163
mamaredo_n <11kakeru30@gmail.com>
2022-02-08 12:37:38 +0900

    update: 本番と開発で適用されるpluginが変更されるように更新

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,102 @@ require('dotenv').config({
   path: `.env.${process.env.NODE_ENV}`
 })
 
+const isProd = process.env.NODE_ENV === "production"
+
+const globalPlugins = [
+  `gatsby-plugin-sitemap`,
+  `gatsby-plugin-react-helmet`,
+  `gatsby-plugin-image`,
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `images`,
+      path: `${__dirname}/src/assets/images`,
+    },
+  },
+  `gatsby-transformer-sharp`,
+  `gatsby-plugin-sharp`,
+  {
+    resolve: `gatsby-plugin-manifest`,
+    options: {
+      name: `gatsby-starter-default`,
+      short_name: `starter`,
+      start_url: `/`,
+      background_color: `#663399`,
+      theme_color: `#663399`,
+      display: `minimal-ui`,
+      icon: `src/assets/gaap-logo.svg`, // This path is relative to the root of the site.
+    },
+  },
+  `gatsby-plugin-postcss`,
+  `gatsby-transformer-inline-svg`,
+  {
+    resolve: "gatsby-transformer-remark",
+    options: {
+      plugins: [
+        {
+          resolve: `gatsby-remark-external-links`,
+          options: {
+            rel: `noopener noreferrer`,
+          }
+        },
+        `gatsby-remark-table-of-contents`,
+        {
+          resolve: `gatsby-remark-autolink-headers`,
+          options: {
+            elements: [`h2`],
+            maintainCase: false,
+            icon: false
+          },
+        },
+      ],
+    },
+  },
+  `gatsby-plugin-typescript`,
+  {
+    resolve: `gatsby-plugin-typegen`,
+    options: {
+      emitSchema: {
+        "src/__generated__/gatsby-introspection.json": true,
+      },
+      emitPluginDocuments: {
+        "src/__generated__/gatsby-plugin-documents.graphql": true,
+      },
+    },
+  },
+  {
+    resolve: `gatsby-source-contentful`,
+    options: {
+      spaceId: process.env.SPACE_ID,
+      accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+    }
+  },
+  {
+    resolve: `gatsby-plugin-google-fonts`,
+    options: {
+      fonts: [
+        `balsamiq sans\:400i,700i,700` // you can also specify font weights and styles
+      ],
+      display: 'swap'
+    }
+  },
+  {
+    resolve: `gatsby-plugin-gatsby-cloud`
+  }
+]
+
+const productionPlugins = [{
+  resolve: "gatsby-plugin-google-gtag",
+  options: {
+    trackingIds: ["UA-198042353-1"],
+    pluginConfig: {
+      head: true,
+    },
+  },
+}]
+
+const plugins = isProd ? [...globalPlugins, ...productionPlugins] : globalPlugins
+
 module.exports = {
   flags: {
     FAST_DEV: true,
@@ -13,101 +109,5 @@ module.exports = {
     author: `Nishimura`,
     siteUrl: `https://gaap.gatsbyjs.io`
   },
-  plugins: [
-    `gatsby-plugin-sitemap`,
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-image`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/assets/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/assets/gaap-logo.svg`, // This path is relative to the root of the site.
-      },
-    },
-    `gatsby-plugin-postcss`,
-    `gatsby-transformer-inline-svg`,
-    {
-      resolve: "gatsby-transformer-remark",
-      options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-external-links`,
-            options: {
-              rel: `noopener noreferrer`,
-            }
-          },
-          `gatsby-remark-table-of-contents`,
-          {
-            resolve: `gatsby-remark-autolink-headers`,
-            options: {
-              elements: [`h2`],
-              maintainCase: false,
-              icon: false
-            },
-          },
-        ],
-      },
-    },
-    `gatsby-plugin-typescript`,
-    {
-      resolve: `gatsby-plugin-typegen`,
-      options: {
-        emitSchema: {
-          "src/__generated__/gatsby-introspection.json": true,
-        },
-        emitPluginDocuments: {
-          "src/__generated__/gatsby-plugin-documents.graphql": true,
-        },
-      },
-    },
-    {
-      resolve: `gatsby-source-contentful`,
-      options: {
-        spaceId: process.env.SPACE_ID,
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-      }
-    },
-    {
-      resolve: `gatsby-plugin-google-fonts`,
-      options: {
-        fonts: [
-          `balsamiq sans\:400i,700i,700` // you can also specify font weights and styles
-        ],
-        display: 'swap'
-      }
-    },
-    {
-      resolve: `gatsby-plugin-gatsby-cloud`
-    },
-    {
-      resolve: `gatsby-plugin-canonical-urls`,
-      options: {
-        siteUrl: `https://gaap.gatsbyjs.io`,
-        stripQueryString: true,
-      },
-    },
-    {
-      resolve: "gatsby-plugin-google-gtag",
-      options: {
-        trackingIds: ["UA-198042353-1"],
-        pluginConfig: {
-          head: true,
-        },
-      },
-    },
-  ],
+  plugins
 }
