4af21490d7b74000d13227c1ad43ee3ec591eb91
mamaredo_n <11kakeru30@gmail.com>
2022-02-08 14:38:58 +0900

    バージョン3への対応

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  purge: ["public/**/*.html", "src/**/*.{js,jsx,ts,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  content: ["public/**/*.html", "src/**/*.{js,jsx,ts,tsx}"],
+  // darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {
